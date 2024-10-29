e229c11e395aa74bf5f46af6f35f3b9e305efff4
SiHyun <there7788@gmail.com>
2024-10-29 20:02:00 +0900

    Feature: 쿠키 파싱 및 CORS 설정, 포트 변경

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(cookieParser());
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN ? [process.env.CORS_ORIGIN] : [],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    exposedHeaders: 'Authorization, Content-Type, Accept',
+  });
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
